682abce90b2e74aa9b6f3142f74527d1c3888077
Monique Hampton <moniquejhampton@yahoo.com>
2017-10-07 10:30:18 -0400

    heroku fix

--- server.js
+++ server.js
@@ -50,8 +50,7 @@ app.use(router);
 // if deployed use the deployed database. Otherwise use the local mongoHeadlines db
 //var db = process.env.MONGODB_URI || "mongodb://localhost/mongoScraper";
 
-mongoose.connect("mongodb://heroku_f2q80qj3:6bk0fdi4222j8842thogj5rmus@ds111895.mlab.com:11895/heroku_f2q80qj3");
-var db = mongoose.connection;
+var db = process.env.MONGODB_URI || "mongodb://heroku_f2q80qj3:6bk0fdi4222j8842thogj5rmus@ds111895.mlab.com:11895/heroku_f2q80qj3";
 
 // Connect Mongoose to db
 mongoose.connect(db, function (error) {
